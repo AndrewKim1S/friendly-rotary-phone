420ee598616e210db4deaa3b6a7b780447dd10e0
Andrew Kim <54419795+AndrewKim1S@users.noreply.github.com>
2025-02-26 21:06:18 -0600

    Add files via upload

--- lab2/assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ lab2/assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,3 +1,4 @@
+
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
@@ -6,67 +7,35 @@ import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 
 import java.util.HashMap;
+import net.floodlightcontroller.packet.MACAddress;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
 {	
-
-	// Class to hold Mac Addr to Interface & time Map 
-	protected class MacInterMap {
-		private HashMap<MACAddress, NameTimePair> map = new HashMap<MACAddress, NameTimePair>();
-		
-		public void insert(MACAddress m, String n, long t) {
-			map.put(m, new NameTimePair(n,t));
-		}
-
-		public boolean containsMAC(MACAddress m) {
-			return map.containsKey(m);
-		}
-
-		public String getName(MACAddress m) {
-			return map.get(m).name;
-		}
-
-		public long getTime(MACAddress m) {
-			return map.get(m).time;
-		}
-
-		// Helper pair class 
-		private class NameTimePair {
-			public String name;		// name of interface
-			public long time;			// last updated
-
-			public NameTimePair(String n, long t) {
-				this.name = n;
-				this.time = t;
-			}
-		}
-	}
-	
-	// Define table for MAC learning
-	public MacInterMap table;
+	private HashMap<MACAddress, Iface> MAC_Inter;
+	private HashMap<MACAddress, Long> MAC_Time;
 
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
 	 */
+	
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
-		table = new MacInterMap();
+		MAC_Inter = new HashMap<MACAddress, Iface>();
+		MAC_Time = new HashMap<MACAddress, Long>();
 	}
 
 
-	// Custom function to check for timeout in table
+	// Check if there is a timeout
 	private boolean timeout(MACAddress m) {
-		long last_updated = table.getTime(src_mac);
+		long last_updated = MAC_Time.get(m);
 		long curr_time = System.currentTimeMillis();
-
-		// Check Timeout && maybe inter == logged
-		return (curr_time - last_updated) / 1000.0 >= 15;
-	}
+		return (curr_time - last_updated) >= 15000;
+	} 
 
 
 	/**
@@ -79,41 +48,28 @@ public class Switch extends Device
 		System.out.println("*** -> Received packet: " +
 				etherPacket.toString().replace("\n", "\n\t"));
 		
-		//Check if mac addr src is in table 
-			// if yes check time - timeout && check if inter == to the one logged
-			// else do learning
-		// Check if mac addr dst is in table 
-			// if yes Check timeout send packet
-			// else broadcast/flood 
-		
 		MACAddress src_mac = etherPacket.getSourceMAC();
 		MACAddress dst_mac = etherPacket.getDestinationMAC();
-		String src_inter = inIface.getName();
-
-		// Check if the MAC Addr src is in table 
-		if(table.containsMAC(src_mac)) {
-			// Check Timeout && maybe inter == logged
-			if(timeout(src_mac)) {
-				table.insert(src_mac, src_inter, curr_time);
-			}
-		} 
-		// do learning
-		else {
-			table.insert(src_mac, src_inter, System.currentTimeMillis());
+		
+		// src mac is in table
+		if(!MAC_Inter.containsKey(src_mac) || timeout(src_mac)) {
+			MAC_Inter.put(src_mac, inIface);
+			MAC_Time.put(src_mac, System.currentTimeMillis());
 		}
 
-		// Check if mac dst is in table && timeout
-		if(table.containsMAC(dst_mac) && !timeout(dst_mac)) {
-			// send packet
-			Iface toIface = interfaces.getInterface(table.getName(dst_mac));
-			sendPacket(etherPacket, toIface);
+		// dst mac is in table
+		if(MAC_Inter.containsKey(dst_mac) && !timeout(dst_mac)) {
+			sendPacket(etherPacket, MAC_Inter.get(dst_mac));
 		}
-		// broadcast/flood the packet
+		// flood/broadcast
 		else {
-			for(String inter_name : interfaces.keySet()) {
-				Iface toIface = interfaces.getInterface(inter_name);
-				sendPacket(etherPacket, toIface);
+			for(Iface iface : interfaces.values()) {
+				if(iface.equals(inIface)) {
+					sendPacket(etherPacket, iface);
+				}
 			}
 		}
+
+		// System.out.println("------------------------------------------------");
 	}
 }
